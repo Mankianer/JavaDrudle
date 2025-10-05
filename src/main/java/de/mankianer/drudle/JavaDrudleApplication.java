5a54618e6df4e8ba59fab4ab2916f82c271e51af
Mankianer <mankianer@gmail.com>
2025-10-05 19:17:36 +0200

    init

---
+++ src/main/java/de/mankianer/drudle/JavaDrudleApplication.java
@@ -0,0 +1,13 @@
+package de.mankianer.drudle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDrudleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaDrudleApplication.class, args);
+    }
+
+}
