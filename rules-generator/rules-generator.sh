1c4ae6144815defa5a244d199961628d90397002
Mankianer <mankianer@gmail.com>
2025-10-12 11:46:20 +0200

    feat(rules): add jinja2 rule generator

---
+++ rules-generator/rules-generator.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# find all subdirectories of the script's directory
+SCRIPT_DIR=$(dirname "$0")
+find "$SCRIPT_DIR" -type d | while read -r dir; do
+  # find all *.j2 file in directory
+  find "$dir" -maxdepth 1 -type f -name '*.j2' | while read -r j2file; do
+    filename=$(basename "$j2file" .j2)
+    foldername=$(basename "$dir")
+    # normalize output path $SCRIPT_DIR/../src/resources/rules/$foldername-${filename}
+
+    jinja2 "$j2file" "$dir/values.yaml" -o "$SCRIPT_DIR/../src/main/resources/rules/${foldername}-${filename}"
+  done
+done
